a271fed0981c82484f8ec2af792e68360e4fb32c
Nehal409 <nehalnasir619@gmail.com>
2025-02-03 18:12:02 +0500

    update send message payload validation

--- src/modules/chat/validations/chat.validation.js
+++ src/modules/chat/validations/chat.validation.js
@@ -1,6 +1,14 @@
 import Joi from "joi";
 
 export const sendMessageSchema = Joi.object({
-  text: Joi.string().optional(),
-  image: Joi.string().optional(),
+  text: Joi.string().optional().allow("", null),
+  image: Joi.string()
+    .pattern(/^data:image\/(png|jpeg|jpg|gif);base64,([A-Za-z0-9+/=]+)$/)
+    .messages({
+      "string.empty": "Profile picture is required.",
+      "string.pattern.base":
+        "Invalid profile picture format. Must be a Base64-encoded image.",
+    })
+    .optional()
+    .allow(null),
 });
